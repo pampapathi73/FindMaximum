e483f70558a3f93dfa01c889fef00ab6047de480
pampapathi73 <pampapathik32@gmail.com>
2021-06-09 20:01:37 +0530

    [pampapathi] Added:  Given three String find maximum

--- FindMaximum/Program.cs
+++ FindMaximum/Program.cs
@@ -11,6 +11,8 @@ namespace FindMaximum
             Console.WriteLine("The maximum value is :" + max);
             double maxFloat = FindMaximumFromThree.MaximumFloatNumber(4.4, 4.9, 5.2);
             Console.WriteLine("The maximum value is :" + maxFloat);
+            string maxString = FindMaximumFromThree.MaximumStringNumber("Apple", "Peach", "Banana");
+            Console.WriteLine("The maximum String value is :" + maxString);
         }
     }
 }
