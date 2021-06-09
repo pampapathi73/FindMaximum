1d01a40619c352afdd3287092f72c18ec90fa88e
pampapathi73 <pampapathik32@gmail.com>
2021-06-09 19:01:42 +0530

    [pampapathi] Added: welcome to find maximum number

---
+++ FindMaximum/FindMaximumFromThree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaximum
+{
+    class FindMaximumFromThree
+    {
+        public static int MaximumIntegerNumber(int firstValue, int SecondValue, int ThirdValue)
+        {
+            if (firstValue.CompareTo(SecondValue) > 0 && firstValue.CompareTo(ThirdValue) > 0)
+                return firstValue;
+            if (SecondValue.CompareTo(firstValue) > 0 && SecondValue.CompareTo(ThirdValue) > 0)
+                return SecondValue;
+            if (ThirdValue.CompareTo(firstValue) > 0 && ThirdValue.CompareTo(SecondValue) > 0)
+                return ThirdValue;
+            else
+                throw new Exception("All numbers are same");
+        }
+    }
+}
---
+++ FindMaximum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FindMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find maximum number");
+            int max = FindMaximumFromThree.MaximumIntegerNumber(4, 7, 6);
+            Console.WriteLine("The maximum value is :" + max);
+        }
+    }
+}
