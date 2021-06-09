c890c7b2eb649e470cdf67a1ef6061827e23398b
pampapathi73 <pampapathik32@gmail.com>
2021-06-09 20:33:44 +0530

    [pampapathi] Added:  ExtendToTakeMoreThan3parameter

--- FindMaximum/Program.cs
+++ FindMaximum/Program.cs
@@ -6,13 +6,15 @@ namespace FindMaximum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Find maximum number");
-            int max = FindMaximumFromThree.MaximumIntegerNumber(4, 6, 4);
-            Console.WriteLine("The maximum value is :" + max);
-            double maxFloat = FindMaximumFromThree.MaximumFloatNumber(4.4, 4.9, 5.2);
-            Console.WriteLine("The maximum value is :" + maxFloat);
-            string maxString = FindMaximumFromThree.MaximumStringNumber("Apple", "Peach", "Banana");
-            Console.WriteLine("The maximum String value is :" + maxString);
+            int[] intArray = { 3, 5, 6 };
+            GenericMaximum1<int> genericMaximum = new GenericMaximum1<int>(intArray);
+            genericMaximum.FindMaximum();
+            double[] doubleArray = { 4.5, 7.2, 9.7 };
+            GenericMaximum1<double> genericDouble = new GenericMaximum1<double>(doubleArray);
+            genericDouble.FindMaximum();
+            string[] stringArray = { "Apple", "Peach", "Banana" }; 
+            GenericMaximum1<string> genericString = new GenericMaximum1<string>(stringArray);
+            genericString.FindMaximum();
         }
     }
 }
