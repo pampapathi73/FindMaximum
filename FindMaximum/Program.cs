7bba01ccb49370a5b9a6abebc7b9f4a1901909f6
pampapathi73 <pampapathik32@gmail.com>
2021-06-09 19:19:52 +0530

    [pampapathi] Added:  GivenMaxNoAtFirst

--- FindMaximum/Program.cs
+++ FindMaximum/Program.cs
@@ -7,7 +7,7 @@ namespace FindMaximum
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Find maximum number");
-            int max = FindMaximumFromThree.MaximumIntegerNumber(4, 7, 6);
+            int max = FindMaximumFromThree.MaximumIntegerNumber(7, 4, 3);
             Console.WriteLine("The maximum value is :" + max);
         }
     }
